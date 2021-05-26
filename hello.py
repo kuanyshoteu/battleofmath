eb3cff5adefd14a3c89d39545360430ee274795f
Kuanysh <kuanyshoteu@gmail.com>
2021-05-26 14:04:24 +0600

    react course gallery

--- hello.py
+++ hello.py
@@ -15,5 +15,5 @@ from whitenoise import WhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cerebrium.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
 
